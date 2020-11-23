66dad6dcf576cc258c78ba66b61179ccbed49258
smrkhan123 <sk862147@gmail.com>
2020-11-23 10:04:36 +0530

    task added

--- ajax.php
+++ ajax.php
@@ -1,22 +1,23 @@
 <?php
-    $id = $_POST['data'];
-    $opt = $_POST['operator'];
-    $tempVar = $_POST['tempVar'];
-    $nextVar = $_POST['nextVar'];
-    if ($opt == "add") {
-        $tempVar = $tempVar + $nextVar;
-    }
-    else if ($opt == "substract") {
-        $tempVar = $tempVar - $nextVar;
-    }
-    else if ($opt == "multiply") {
-        $tempVar = $tempVar * $nextVar;
-    }
-    else if ($opt == "divide") {
+$id = $_POST['data'];
+$opt = $_POST['operator'];
+$tempVar = $_POST['tempVar'];
+$nextVar = $_POST['nextVar'];
+if ($opt == "add") {
+    $tempVar = $tempVar + $nextVar;
+} else if ($opt == "substract") {
+    $tempVar = $tempVar - $nextVar;
+} else if ($opt == "multiply") {
+    $tempVar = $tempVar * $nextVar;
+} else if ($opt == "divide") {
+    if ($nextVar == 0) {
+        $tempVar = "";
+    } else {
         $tempVar = $tempVar / $nextVar;
     }
-    $opt = $id;
-    $nextVar = " ";
-    $arr = array($tempVar, $opt, $nextVar, $id);
-    echo json_encode(array("arr" => $arr));
+}
+$opt = $id;
+$nextVar = " ";
+$arr = array($tempVar, $opt, $nextVar, $id);
+echo json_encode(array("arr" => $arr));
 ?>
